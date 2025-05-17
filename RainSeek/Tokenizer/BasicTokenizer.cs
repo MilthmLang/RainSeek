94bdfda30703693eeb13faede89f9c0115fa7fe1
Eric_Lian <git@superexercisebook.com>
2025-05-17 11:15:10 +0800

    feat: workaround in Unity for U+1680

--- RainSeek/Tokenizer/BasicTokenizer.cs
+++ RainSeek/Tokenizer/BasicTokenizer.cs
@@ -1,15 +1,43 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using RainSeek.Indexing;
 
 namespace RainSeek.Tokenizer
 {
     public class BasicTokenizer : ITokenizer
     {
-        public IReadOnlyList<string> Delimiters { get; set; } = new[] { " " };
+        public IReadOnlyList<string> Delimiters { get; set; } = new[]
+        {
+            "\u0020", "\u00A0",
+            "\u1680",
+            "\u2000", "\u2001", "\u2002", "\u2003",
+            "\u2004", "\u2005", "\u2006", "\u2007",
+            "\u2008", "\u2009", "\u200A",
+            "\u202F", "\u205F", "\u3000"
+        };
+
         public Func<TokenModel, bool> Predictor { get; set; } = _ => true;
 
         public bool CaseSensitive { get; set; } = false;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool MetDelimiter(string content, int index, string delimiter)
+        {
+            string p = content.Substring(index);
+
+            if (p.Length == 0)
+            {
+                return false;
+            }
+
+            if (p.Length > 0 && delimiter.Length == 1)
+            {
+                return p[0] == delimiter[0];
+            }
+
+            return content.Substring(index).StartsWith(delimiter, StringComparison.InvariantCulture);
+        }
 
         public IReadOnlyList<TokenModel> Tokenize(string content)
         {
@@ -21,7 +49,7 @@ namespace RainSeek.Tokenizer
                 bool isDelimiter = false;
                 foreach (var delimiter in Delimiters)
                 {
-                    if (content.Substring(index).StartsWith(delimiter))
+                    if (MetDelimiter(content, index, delimiter))
                     {
                         index += delimiter.Length;
                         isDelimiter = true;
@@ -37,7 +65,7 @@ namespace RainSeek.Tokenizer
                     bool atDelimiter = false;
                     foreach (var delimiter in Delimiters)
                     {
-                        if (content.Substring(index).StartsWith(delimiter))
+                        if (MetDelimiter(content, index, delimiter))
                         {
                             atDelimiter = true;
                             break;
